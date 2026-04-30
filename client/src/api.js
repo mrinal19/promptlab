3722ae53d0314343d65870624072d2cd0d5c1784
Mrinal Mishra <144545950+mrinal19@users.noreply.github.com>
2026-04-30 14:34:58 +0530

    Create api.js

---
+++ client/src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export const evaluationsApi = {
+  getAll: (params = {}) => api.get('/evaluations', { params }).then(r => r.data),
+  getOne: (id) => api.get(`/evaluations/${id}`).then(r => r.data),
+  create: (data) => api.post('/evaluations', data).then(r => r.data),
+  update: (id, data) => api.patch(`/evaluations/${id}`, data).then(r => r.data),
+  delete: (id) => api.delete(`/evaluations/${id}`).then(r => r.data),
+};
+
+export const labelsApi = {
+  getAll: () => api.get('/labels').then(r => r.data),
+  create: (data) => api.post('/labels', data).then(r => r.data),
+};
+
+export const statsApi = {
+  get: () => api.get('/stats').then(r => r.data),
+};
+
+export default api;
