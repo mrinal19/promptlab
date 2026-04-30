bdcf261030e589fc00f3c4da4b4a21ca4c96c2f1
Mrinal Mishra <144545950+mrinal19@users.noreply.github.com>
2026-04-30 16:02:53 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,25 +1,24 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
 
 export const evaluationsApi = {
-  getAll: (params = {}) => api.get('/evaluations', { params }).then(r => r.data),
-  getOne: (id) => api.get(`/evaluations/${id}`).then(r => r.data),
-  create: (data) => api.post('/evaluations', data).then(r => r.data),
-  update: (id, data) => api.patch(`/evaluations/${id}`, data).then(r => r.data),
-  delete: (id) => api.delete(`/evaluations/${id}`).then(r => r.data),
+  getAll: (params = {}) => api.get('/api/evaluations', { params }).then(r => r.data),
+  getOne: (id) => api.get(`/api/evaluations/${id}`).then(r => r.data),
+  create: (data) => api.post('/api/evaluations', data).then(r => r.data),
+  update: (id, data) => api.patch(`/api/evaluations/${id}`, data).then(r => r.data),
+  delete: (id) => api.delete(`/api/evaluations/${id}`).then(r => r.data),
 };
 
 export const labelsApi = {
-  getAll: () => api.get('/labels').then(r => r.data),
-  create: (data) => api.post('/labels', data).then(r => r.data),
+  getAll: () => api.get('/api/labels').then(r => r.data),
+  create: (data) => api.post('/api/labels', data).then(r => r.data),
 };
 
 export const statsApi = {
-  get: () => api.get('/stats').then(r => r.data),
+  get: () => api.get('/api/stats').then(r => r.data),
 };
-
