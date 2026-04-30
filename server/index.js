575f0053cd6d243e32a44b49c361a421197d36b9
Mrinal Mishra <144545950+mrinal19@users.noreply.github.com>
2026-04-30 15:10:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -63,6 +63,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
